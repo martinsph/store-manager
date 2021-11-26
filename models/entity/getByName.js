f42c47c114229506639900dd45b23c52ea4c507d
martinsph <ph.mmartins@gmail.com>
2021-11-26 00:16:13 -0300

    test req 1

--- models/entity/getByName.js
+++ models/entity/getByName.js
@@ -1,10 +1,10 @@
 const connection = require('../connection');
 
 // função recebe a coleção e o filtro por nome.
-module.exports = async (collection, name) => {
+module.exports = async (collection, _name) => {
   try {
     const connecting = await connection();
-    return connecting.collection(collection).findOne(name);
+    return connecting.collection(collection).findOne({ name: _name });
   } catch (error) {
     console.log(error);
   }
--- services/products/create.js
+++ services/products/create.js
@@ -10,27 +10,34 @@ const errors = {
     status: 422, code: 'invalid_data', message: 'Product already exists' },
   invalidQty: {
     status: 422, code: 'invalid_data', message: '"quantity" must be larger than or equal to 1' },
+  invalidSQtyType: {
+    status: 422, code: 'invalid_data', message: '"quantity" must be a number' },
 };
 
 const isValidName = (name) => {
   if (name.length <= MAX_LENGTH) throw errors.invalidName;
 };
 
-const nameExists = (name) => {
-  const checkName = getByName(name);
+const nameExists = async (name) => {
+  const checkName = await getByName(name);
   if (checkName !== null) throw errors.productExists;
 };
 
 const isValidQty = (quantity) => {
-  if (!Number.isInteger(quantity) || quantity <= 0) throw errors.invalidQty;
+  if (quantity < 1) throw errors.invalidQty;
+};
+
+const isValidQtyType = (quantity) => {
+  if (typeof quantity !== 'number') throw errors.invalidSQtyType; 
 };
 
 module.exports = async (newProduct) => {
   const { name, quantity } = newProduct;
 
   isValidName(name);
-  nameExists(name);
+  await nameExists(name);
   isValidQty(quantity);
+  isValidQtyType(quantity);
 
   product.create(newProduct);
 };
